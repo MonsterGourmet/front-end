8c4fa166f6e9847c26e8ae30b92a0334174f4bbd
Bruno120Ab <abreubruno499@gmail.com>
2023-08-29 14:46:46 -0300

    feat: add component Header

--- src/components/Header/styled.tsx
+++ src/components/Header/styled.tsx
@@ -4,14 +4,18 @@ import styled from 'styled-components';
 
 
 export const SHeader = styled.header`
-  height: 150px;
+  height: 140px;
   width: 100%;
 
-  padding: 0 10px;
+  padding: 15px 15px 5px 15px;
   border: 1px solid red;
   
+  justify-content: space-between;
   flex-direction: row;
-  align-items: center;
+  align-items: flex-start;
   display: flex;
+
+  transition: 200ms linear;
+
 `;
 
