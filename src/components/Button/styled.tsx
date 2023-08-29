d39c4a6cbcb400718f9124a8b5c5d33e84c0c8bc
Bruno120Ab <abreubruno499@gmail.com>
2023-08-29 09:44:06 -0300

    feat: add component

---
+++ src/components/Button/styled.tsx
@@ -0,0 +1,15 @@
+'use client'
+import styled from 'styled-components';
+import css from 'styled-jsx/css';
+
+
+
+export const SButton = styled.button`
+  width: 50px;
+  height: 50px;
+
+  /* color: ${props => props.theme.main};
+  border: 2px solid ${props => props.theme.main}; */
+
+`;
+
