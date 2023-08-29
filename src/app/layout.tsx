b7d519a47df8846730f2fa0fe603ed16c56b8368
Bruno120Ab <abreubruno499@gmail.com>
2023-08-29 09:40:40 -0300

    feat: add globalStyles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
-import './globals.css'
-import type { Metadata } from 'next'
+import StyledConfig from '@/styles/_styled.config'
+import GlobalStyle from '@/styles/_globalStyled'
+
 import { Inter } from 'next/font/google'
+import type { Metadata } from 'next'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <GlobalStyle />
+        <StyledConfig>{children}</StyledConfig>
+        </body>
     </html>
   )
 }
