a5951252f43010ed8ae0379fa2d7e66fb2f54568
Bruno120Ab <abreubruno499@gmail.com>
2023-08-29 13:34:46 -0300

    fix: widht, colors and heights

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,12 +1,39 @@
 'use client'
 
 import Image from "next/image";
-import { SFigure } from "./styled";
+import Link from "next/link";
 
-export function Logo({Profile}: any) {
+import { SBall, SFigure, SIconCart } from "./styled";
+
+import { BsCart, BsCartX } from "react-icons/bs";
+
+import { ILogo, Type } from "./types";
+
+export function Logo({Profile, Status}: ILogo): JSX.Element {
      return (
-          <SFigure>
-               <Image src={Profile} alt="Logo"/>
+          <SFigure Status={Status}>
+               <Link href="https://www.linkedin.com/in/brunoabreudev/">
+                    <Image src={Profile} alt="Logo" />
+               </Link>
           </SFigure>
      )
+}
+
+export function IconCart({Number, Status}: Type): JSX.Element {
+     const Open = (Number: number, Status: 'Open' | 'Close'): JSX.Element => (
+          <SIconCart status={Status}>
+               <BsCart></BsCart>
+               <SBall>{Number}</SBall>
+          </SIconCart>
+     )
+
+     const Close = ( Status: 'Open' | 'Close'): JSX.Element => (
+          <SIconCart status={Status}>
+               <BsCartX></BsCartX>
+          </SIconCart>
+     )
+
+     const TypeCart = Status === 'Open' ? Open(Number, Status) : Close(Status)
+
+     return TypeCart
 }
---
+++ src/components/Icons/types.ts
@@ -0,0 +1,18 @@
+import { StaticImageData } from "next/image";
+
+export interface iButton  {
+     status: 'Open' | 'Close';
+}
+
+export interface ILogo {
+     Profile: StaticImageData,
+     Status: 'On' | 'Off';
+}
+
+export interface Type {
+     Number: number;
+     Status: 'Open' | 'Close'
+}
+
+export type ILogoOnly = Pick<ILogo, 'Status'>
+
