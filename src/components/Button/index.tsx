b1371e2edbefabecd4f1d820ef5d8c6df82f2688
Bruno120Ab <abreubruno499@gmail.com>
2023-09-02 00:00:13 -0300

    feat: add new Button the tell

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,17 @@
-import { SButton } from "./styled"
+import { SButton, SButtonTell } from "./styled"
+
+interface IButtonContact {
+    text: string
+}
 
 export function Button() {
     return (
           <SButton >Olá</SButton>
+    )
+}
+
+export function ButtonContact({text}:IButtonContact) {
+    return (
+        <SButtonTell>{text}</SButtonTell>
     )
 }
--- src/components/Button/styled.tsx
+++ src/components/Button/styled.tsx
@@ -13,3 +13,21 @@ export const SButton = styled.button`
 
 `;
 
+export const SButtonTell = styled.button`
+  height: 99%;
+  width: 100px;
+
+  border-radius: 0px 20px 20px 0px;
+  border: 1px solid transparent;
+  
+  background-color: var(--color-White);
+  color: var(--color-font);
+
+  position: absolute;
+  right: 0;
+
+  font-weight: 500;
+  font-size: 15px;
+
+  text-align: center;  
+`
