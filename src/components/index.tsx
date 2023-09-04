35f02cfa951bd2618e7ccd24fe90cc31d027e8bd
Bruno120Ab <abreubruno499@gmail.com>
2023-09-04 18:54:13 -0300

    Revert "fix: Clean"
    This reverts commit 4aa2f7cc9ab5fcee0e5803c3e03823adba23501f.

--- src/components/index.tsx
+++
@@ -1,6 +0,0 @@
-import { Information } from '@/components/Info'
-import { Categories } from '@/components/Categories'
-import { Header } from '@/components/Header'
-import { Menu } from "./Menu";
-
-export { Menu, Header, Categories, Information }
