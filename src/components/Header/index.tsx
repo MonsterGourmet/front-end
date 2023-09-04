07e7d2fa341f54bef49be773dc4f69de767e56b1
Bruno120Ab <abreubruno499@gmail.com>
2023-09-04 18:55:58 -0300

    feat: test in time

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -18,15 +18,15 @@ export function Header() {
     
     const [ stts, setStts] = useState<'Open' | 'Close' >('Close')
 
-    setInterval(()=>setMinute(time.getMinutes()),500)
+    setInterval(()=>setMinute(time.getMinutes()),58000)
 
     // Troca minuto por hora, quando for fechar a compra verifique a hora.
     useEffect(() => {
-        if (minute >= 17 && minute < 29)  {
+        if (minute >= 17 && minute < 23)  {
             setStts('Open');
         } 
 
-        if( minute >= 30){
+        if( minute >= 23){
             setStts('Close');
         }
     }, [minute]);
