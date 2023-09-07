f2225368c763b59dea53e985fd3277def28ed734
Bruno120Ab <abreubruno499@gmail.com>
2023-09-06 21:27:35 -0300

    feat: Add Menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
-import { Categories } from "@/components/Categories"
+import { Menu } from "@/components/Menu"
 import { Header } from "@/components/Header"
+import { Categories } from "@/components/Categories"
 import { Information } from "@/components/Info"
+
+//TODOS: Clean Code, Organization, Desacoplamento, Details
 
 const Home = () => (
   <>
     <Header/>
     <Information />
     <Categories />
+    <Menu />
   </>
 )
 
