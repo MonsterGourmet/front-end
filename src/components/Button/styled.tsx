3969a491da0be0b18ea0719b8b4a604d23f48492
Bruno120Ab <abreubruno499@gmail.com>
2023-09-06 21:36:32 -0300

    feat: Add Button (incomplete)

--- src/components/Button/styled.tsx
+++ src/components/Button/styled.tsx
@@ -1,13 +1,12 @@
 'use client'
+
 import styled from 'styled-components';
+
+import { ButtonProps } from './types';
 
 export const SButton = styled.button`
   width: 50px;
   height: 50px;
-
-  /* color: ${props => props.theme.main};
-  border: 2px solid ${props => props.theme.main}; */
-
 `;
 
 export const SButtonTell = styled.button`
@@ -27,4 +26,65 @@ export const SButtonTell = styled.button`
   font-size: 15px;
 
   text-align: center;  
+`
+
+export const SButtonAdd = styled.button`
+  height: 42px;
+  width: 46px;
+
+  border-radius: 100%;
+  border: 1px solid var(--color-Brand1);
+
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+  display: flex;
+
+  .iconPlus{
+    background-color: transparent;
+    color: var(--color-Brand1);
+
+    font-size: 32px;
+  }
+  .iconAdded{
+    background-color: transparent;
+    color: var(--color-Brand1);
+
+    font-size: 32px;
+  }
+
+`
+
+export const SButtonOption = styled.button<ButtonProps>`
+     height: 40px;
+     min-width: 130px;
+
+     transition: 200ms linear;
+
+     border-radius: 20px;
+     border: 1px solid var(--color-Brand1);
+
+     box-shadow: 0px 1px 1px 0px var(--color-DropShow);
+
+     background-color: ${(props) => (props.$isSelected !== 'all' ? 'var(--color-Brand1)': 'var(--color-White)')};
+     color: var(--color-Brand1);
+
+     justify-content: center;
+     align-items: center;
+     display: flex;
+
+     cursor: pointer;
+
+     .Icon{          
+          height: 40px;
+          width: 50px;
+
+          border-radius: 100%;
+
+          color: var(--color-Brand1);
+
+          animation:slide-in-blurred-right 250ms both;
+
+          overflow: hidden;
+     }
 `
