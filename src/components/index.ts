73e15f6af5252a8039eee95568b72e7c465e5c74
Bruno120Ab <abreubruno499@gmail.com>
2023-09-08 10:16:23 -0300

    fix: import and exports Components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import { Header } from "./Header";
+import { Information } from "./Info";
+import { Categories } from "./Categories";
+import { Menu } from "./Menu";
+import { Footer } from "./Footer";
+
+export { Header, Information, Categories, Menu, Footer}
