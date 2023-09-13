99a31995658369649df26e907de13d3381356722
Bruno120Ab <abreubruno499@gmail.com>
2023-09-13 11:24:59 -0300

    fix: resolve bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Categories, Footer, Header, Information, Menu } from "@/components"
 import { ModalVisibilityControl } from "@/components/Modal"
+
 import HomeProvider from "@/providers/ScreenHome.Context"
 
 //TODOS: Semantic, Align, Clean Code, Organization, Desacoplamento, Details, DefaultCode(Prettier and Eslinter), 'use client', CSS Advanced.
