660a14369b39e59ca296cef9380c046e06bc2cbb
Bruno120Ab <abreubruno499@gmail.com>
2023-10-18 10:28:13 -0300

    feat: add products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Categories, Footer, Header, Information, Menu } from "@/components"
 import { ModalVisibilityControl } from "@/components/Modal"
 
 //TODOS: Semantic, Align, Clean Code, Organization, Desacoplamento, Details, DefaultCode(Prettier and Eslinter), 'use client', CSS Advanced.
+//TODOs: Close Modal.
 
 const ScreenHome = () => (
   <>
